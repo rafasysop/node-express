302ec0245fdbf7c0f2060f15c9a2bdb44ea732de
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-10 20:56:00 -0300

    connectando com mysql server

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./db/conection')
 
-const app = customExpress()
+conexao.connect((error) => {
+  if (error) console.log(error)
+  else {
+    console.log('conectado com sucesso in mysql');
+    const app = customExpress()
 
-const porta = process.env.PORT_APP || 21047
-
-app.listen(porta, () => console.log(`Servidor rodando na porta.: ${porta}`))
+    const porta = process.env.PORT_APP || 21047
+    
+    app.listen(porta, () => console.log(`Servidor rodando na porta.: ${porta}`))
+  }
+})
