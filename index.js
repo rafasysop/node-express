06bf659c299e77b5383b7be8db5c9416a826459c
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-10 14:15:51 -0300

    change

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = customExpress()
 
 const porta = process.env.PORT_APP || 21047
 
-app.listen(porta, () => console.log(`Servidor rodando na porta: ${porta}`))
+app.listen(porta, () => console.log(`Servidor rodando na porta.: ${porta}`))
