d593990648c8b6394818643d22dfeecfbf749c9a
Rafael Moura <rafaelcostamoura@gmail.com>
2021-12-10 14:09:51 -0300

    change port using env

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const customExpress = require('./config/customExpress')
 
 const app = customExpress()
 
-app.listen(21047, () => console.log('Servidor rodando na porta 21047'))
+const porta = process.env.PORT_APP || 3005
+
+app.listen(porta, () => console.log(`Servidor rodando na porta: ${porta}`))
